13b10c9135d32d7de0cef9928f7d22d2d9898fe4
root <root@web.fritz.box>
2016-07-31 12:16:58 +0200

    + passwords and input view

--- api.php
+++ api.php
@@ -1,10 +1,49 @@
 <?php
-	header("Content-Type: text/json");
-	$router->Route("/getpasswords/", function($php){
+header("Content-Type: text/json");
+$db = new DB();
+$router->Route("/getpasswords/", function($php) use($db){
 
-        $db = new DB();
-        $query = "Select * from auth";
-		$stmt = $db->conn->query($query);
-		$result = $stmt->fetchAll(PDO::FETCH_CLASS);
-		var_dump($result);
-    });
+    $query = "Select distinct password, (Select count(id) from auth where password = a.password) as count from auth a order by count desc limit 10;";
+	$stmt = $db->conn->query($query); 
+	$result = $stmt->fetchAll(PDO::FETCH_CLASS);
+	$got = [];
+	foreach($result as $data){
+		$got[] = $data;
+	}
+	echo json_encode($got);
+});
+$router->Route("/getgeoip/", function($php) use($db){
+
+    $query = "Select distinct ip, (Select count(id) from sessions where ip = s.ip) as count from sessions s  order by count desc limit 10;";
+	$stmt = $db->conn->query($query); 
+	$result = $stmt->fetchAll(PDO::FETCH_CLASS);
+	$got = array();
+	foreach($result as $data){
+		$obj = new \stdClass();
+		$obj->IP = $data->ip;
+		$obj->Count = $data->count;
+		$geoip = file_get_contents("http://ip-api.com/json/".$data->ip);
+		$geoip = json_decode($geoip);
+		$obj->Latitude = $geoip->lat;
+		$obj->Longitude = $geoip->lon;
+		$obj->City = $geoip->city;
+		$obj->Region = $geoip->regionName;
+		$obj->Country = $geoip->country;
+		$got[] = $obj;
+	}
+	echo json_encode($got);
+});
+$router->Route("/getinputs/", function($php) use($db){
+
+    $query = "Select input,success from input limit 10;";
+	$stmt = $db->conn->query($query); 
+	$result = $stmt->fetchAll(PDO::FETCH_CLASS);
+	$got = array();
+	foreach($result as $data){
+		$obj = new \stdClass();
+		$obj->Input = $data->input;
+		$obj->Success = $data->success ? "true" : "false";
+		$got[] = $obj;
+	}
+	echo json_encode($got);
+});
--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <?php
 	define("__BUTTERPHP_ALL__","R");
+    define("__APNAME__","Cowrie-Web");
 	require_once "vendor/squarerootfury/butterphp/src/autoload.php";
 	$router = new \BTRRouter();
     require_once "db.php";
     //Note: You _always_  need a default route for /
     $router->Route("/", function($php){
+    	header("Content-Type:text/html");
         //do something. the $php parameter is very important
         //e. g. you can access a $_GET param "foo", with $php->get->foo
+        $main = "./views/main.html";
+        include "./views/template.html";
+    });
+    $router->Route("/geoip/", function($php){
+    	header("Content-Type:text/html");
+        $main = "./views/geoip.html";
+        include "./views/template.html";
+    });
+    $router->Route("/passwords/", function($php){
+        header("Content-Type:text/html");
+        $main = "./views/passwords.html";
+        include "./views/template.html";
+    });
+    $router->Route("/inputs/", function($php){
+        header("Content-Type:text/html");
+        $main = "./views/inputs.html";
         include "./views/template.html";
     });
     require_once "api.php";
